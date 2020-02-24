73a43b32521fb9b280de0a6827a142b239cff0b9
Viacheslav Denisenko <vyacheslavden@mail.ru>
2020-02-25 01:37:58 +0300

    fy

--- example.cpp
+++ example.cpp
@@ -9,6 +9,7 @@ int main()
    string s2;
    cout << "Enter your name: ";
    cin >> s1;
+   cout << s1.length() << endl; // line to remove from commit
    cout << "Now enter you last name: ";
    cin >> s2;
 	cout << "Hello, " << s1 << " " << s2 << endl;
