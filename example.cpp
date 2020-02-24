b1ad07f1cd113447f7aba81fc360f3ad13f3e07a
Viacheslav Denisenko <vyacheslavden@mail.ru>
2020-02-25 01:26:52 +0300

    Give a personal hello.

--- example.cpp
+++ example.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello world" << endl;
+   string s;
+   cout << "Enter your name: ";
+   cin >> s;
+   cout << s.length() << endl; // line to remove from commit
+	cout << "Hello, " << s << endl;
 	return 0;
 }
